2c8f6db0b2989eb3da3d7c7336912649f4784ded
YannPaim <161754380+YannPaim@users.noreply.github.com>
2024-04-03 20:41:10 -0300

    numero aleatorio c#.cs

--- Aula 6/numero aleatorio c#.cs
+++ Aula 6/numero aleatorio c#.cs
@@ -3,19 +3,19 @@ class HelloWorld {
   static void Main() {
       int chute;
       int tentativas = 0;
-      Random numAleatorio = new Random();
-      int valorInteiro = numAleatorio.Next(1, 100);
+      Random numaleatorio = new Random();
+      int numsorteado = numaleatorio.Next(1, 100);
       
         do {
         Console.WriteLine("Escreva um número de 1 a 100 para ver se você acerta ou não: ");
         chute = Convert.ToInt32(Console.ReadLine());
       
-        if (chute > valorInteiro) 
+        if (chute > numsorteado) 
         {
         Console.WriteLine("O número sorteado é menor digite outro numero");
         } 
 
-        else if (chute < valorInteiro) 
+        else if (chute < numsorteado) 
         {
         Console.WriteLine("O número sorteado é maior digite outro numero");
         }
@@ -23,7 +23,7 @@ class HelloWorld {
         tentativas++;
 
         }   
-        while (chute != valorInteiro);
+        while (chute != numsorteado);
         Console.WriteLine("voce acertou");
         Console.WriteLine("voce precisou de " + tentativas + " tentativas para acertar");
   }
